e860e93ff7277269a51b3109957510a042682167
1footguy <jonatasdeassis@gmail.com>
2024-01-30 14:58:06 -0300

    header

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,9 +1,11 @@
-function confirmarDelete(event) {
-    document.getElementById("delete").addEventListener("submit", confirmarDelete);
-let confirmar = confirm("Você tem certeza que deseja excluir este cliente?");
-if (confirmar) {
-    alert("Cliente deletado");
-} else{
-    event.preventDefault;
-}
+const BurgerBtn = document.querySelector('.burger_btn');
+const BurgerBtnIcon = document.querySelector('.burger_btn i');
+const BurgerMenu = document.querySelector('.burger_menu');
+
+BurgerBtn.onclick = function () {
+    BurgerMenu.classList.toggle('open');
+    const isOpen = BurgerMenu.classList.contains('open');
+    BurgerBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
 }
